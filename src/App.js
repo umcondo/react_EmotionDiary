08894db2d995b7530d98c719e4879723d1218f7b
umcondo <superpowerkdg@gmail.com>
2022-04-25 10:55:28 +0900

    create app and page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Diary from "./Pages/Diary";
+import Edit from "./Pages/Edit";
+import New from "./Pages/New";
+import RouteTest from "./components/RouteTest";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>감정일기</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Edit.js
@@ -0,0 +1,44 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const navigate = useNavigate();
+
+  const id = searchParams.get("id");
+  console.log("id:", id);
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기수정페이지 입니다</p>
+      <button
+        onClick={() =>
+          setSearchParams({
+            who: "강대국",
+          })
+        }
+      >
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        홈으로가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
